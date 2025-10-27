f1c710fe90255f0fb25d189093ef0cd43a8105e1
Ilene_Dragon <keali.irene@gmail.com>
2025-10-27 10:45:44 -0700

    Added string function

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
+void say(string s);
 
 int main() {
     cout << "Greeting..." << endl;
     cout << "Hello World!\n";
     return 0;
+}
+
+void say(string s) 
+{
+    cout << s << endl;
 }
